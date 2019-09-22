705977969dbf45b7342fc7d862aece11da8634d9
StorageOcean <victorkevin.ms@gmail.com>
2019-09-22 12:44:08 -0500

    Test .Net Core API
    Test .Net Core API

---
+++ WebApplication1/WebApplication1/Model/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Model
+{
+    public partial class Productos
+    {
+        [Key]
+        [Column("idProducto")]
+        public int IdProducto { get; set; }
+        [StringLength(100)]
+        public string Descripcion { get; set; }
+    }
+}
